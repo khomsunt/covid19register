2dddb8f0e939142951563ad973150411890213e2
teetawat147 <teetawat147@hotmail.com>
2020-12-23 15:08:02 +0700

    config2

--- include/config.php
+++ include/config.php
@@ -1,9 +1,9 @@
 <?php
-
 return array(
-    'host' => 'localhost',
+    'host' => '203.157.177.7',
     'username' => 'root',
+    'password' => '64127427',
+    'port' => '3306',
+    'db' => 'covid19register'
 );
-
-
 ?>
