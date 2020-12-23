bb333f1db9d6092c7f1d5ab22b7ad2c4df88aa2f
jackskn <smatchai.y@hotmail.co.th>
2020-12-23 16:12:15 +0700

    u

--- main/ajaxTest.php
+++ main/ajaxTest.php
@@ -1,9 +1,9 @@
 <?php
 include('../include/config.php');
 
-$sql="select * from ".$_POST["query_table"]." ";
+$sql="select * from ".$_REQUEST["query_table"]." ";
 if ($_POST["query_where"] != "") {
-    $sql.=" where ".$_POST["query_where"];
+    $sql.=" where ".$_REQUEST["query_where"];
 }
 $obj=$connect->prepare($sql);
 $obj->execute();
