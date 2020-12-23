8c3280b7efd8d14d69f13e3781769e6effeec125
teetawat147 <teetawat147@hotmail.com>
2020-12-23 16:40:26 +0700

    new register

--- main/register.php
+++ main/register.php
@@ -12,7 +12,7 @@ include('../include/config.php');
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Jekyll v4.1.1">
-    <title>Floating labels example · Bootstrap</title>
+    <title>register</title>
 
     <script src="../js/jquery-3.5.1.min.js"></script>
 
@@ -48,7 +48,7 @@ $obj->execute();
 $rows=$obj->fetchAll(PDO::FETCH_ASSOC);
 // echo json_encode($row_oapp, JSON_UNESCAPED_UNICODE);
 for ($i=0;$i<count($rows);$i++) {
-  echo "<br>- ".$rows[$i]["user_login"]."|".$rows[$i]["user_password"];
+  //echo "<br>- ".$rows[$i]["user_login"]."|".$rows[$i]["user_password"];
 }
 ?>
 <script>
@@ -62,37 +62,154 @@ $.ajax({
   console.log(xdata);
 });
 </script>
+
+<script>
+  $(document).ready(function () {
+      $('.datepicker').datepicker({
+          format: 'dd/mm/yyyy',
+          todayBtn: true,
+          language: 'th',//เปลี่ยน label ต่างของ ปฏิทิน ให้เป็น ภาษาไทย   (ต้องใช้ไฟล์ bootstrap-datepicker.th.min.js นี้ด้วย)
+          thaiyear: true //Set เป็นปี พ.ศ.
+      }).datepicker("setDate", "0");//กำหนดเป็นวันปัจุบัน
+  });
+</script>
 <div class="container">
+<h2 style="text-align:center">ลงทะเบียนรายงานตัวล่วงหน้า</h2>
 <form>
   <div class="form-group">
-    <label for="exampleFormControlInput1">Email address</label>
-    <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Example select</label>
-    <select class="form-control" id="exampleFormControlSelect1">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+    <label for="exampleFormControlSelect1">คำนำหน้าชื่อ</label>
+    <select class="form-control" id="pname">
+      <option>นาย</option>
+      <option>นาง</option>
+      <option>นางสาว</option>
+      <option>ด.ช.</option>
+      <option>ด.ญ.</option>
     </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect2">Example multiple select</label>
-    <select multiple class="form-control" id="exampleFormControlSelect2">
-      <option>1</option>
-      <option>2</option>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
+    </div>
+
+    <div class="form-group">
+      <label for="exampleFormControlInput1">ชื่อ</label>
+      <input type="email" class="form-control" id="fname" placeholder="ชื่อ">
+    </div>
+
+    <div class="form-group">
+      <label for="exampleFormControlInput1">นามสกุล</label>
+      <input type="email" class="form-control" id="lname" placeholder="นามสกุล">
+    </div>
+
+    <div class="form-group">
+      <label for="exampleFormControlInput1">เลขบัตรประจำตัวประชาชน</label>
+      <input type="email" class="form-control" id="cid" placeholder="เลขบัตรประจำตัวประชาชน">
+    </div>
+  
+    <div class="form-group">
+      <label for="exampleFormControlInput1">เบอร์โทรศัพท์</label>
+      <input type="email" class="form-control" id="tel" placeholder="เบอร์โทรศัพท์">
+    </div>
+
+    <div class="form-group">
+      <label for="exampleFormControlInput1">ที่อยู่ปัจจุบัน</label>
+      <input type="email" class="form-control" id="moo_out" placeholder="ที่อยู่ปัจจุบัน">
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">ตำบล</label>
+    <select class="form-control" id="district_out_code">
+      <option>ธาตุเชิงชุม</option>
+      <option>ธาตุนาเวง</option>
+      <option>เชียงเครือ</option>
+      <option>ธาตุดุม</option>
     </select>
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlTextarea1">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-  </div>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">อำเภอ</label>
+    <select class="form-control" id="amphur_out_code">
+      <option>เมืองสกลนคร</option>
+      <option>กุสุมาลย์</option>
+      <option>กุดบาก</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">จังหวัด</label>
+    <select class="form-control" id="changwat_out_code">
+      <option>สกลนคร</option>
+      <option>กาฬสินธุ์</option>
+      <option>นครพนม</option>
+      <option>บึงกาฬ</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">อาชีพ</label>
+    <select class="form-control" id="occupation_id">
+      <option>ขายอาหารทะเล</option>
+      <option>คนขับรถ</option>
+      <option>ร่ำรวย</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+      <label for="inputdatepicker" class="col-md-2 control-label">datepicker</label>
+      <div class="col-md-10">
+        <input id="inputdatepicker" class="datepicker" data-date-format="mm/dd/yyyy">
+    </div>
+
+    <div class="form-group">
+      <label for="exampleFormControlInput1">ประวัติการเคยสัมผัสผู้ป่วย</label>
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="touch_history" value="1" >
+      <label for="exampleRadios1">
+        เคยสัมผัส
+      </label>
+    </div>
+
+    <div class="form-check">
+      <input class="form-check-input" type="radio" name="exampleRadios" id="touch_history" value="2" >
+      <labe for="exampleRadios1">
+        ไม่เคยสัมผัส
+      </label>
+    </div>
+    </div>
+
+<div class="form-group">
+      <label for="exampleFormControlInput1">เข้าพำนักที่บ้านเลขที่</label>
+      <input type="email" class="form-control" id="house_in_no" placeholder="เข้าพำนักที่บ้านเลขที่">
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">เข้าพำนักที่ตำบล</label>
+    <select class="form-control" id="district_in_code">
+      <option>ธาตุเชิงชุม</option>
+      <option>ธาตุนาเวง</option>
+      <option>เชียงเครือ</option>
+      <option>ธาตุดุม</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">เข้าพำนักที่อำเภอ</label>
+    <select class="form-control" id="amphur_in_code">
+      <option>เมืองสกลนคร</option>
+      <option>กุสุมาลย์</option>
+      <option>กุดบาก</option>
+    </select>
+    </div>
+
+    <div class="form-group">
+    <label for="exampleFormControlSelect1">เข้าพำนักที่จังหวัด</label>
+    <select class="form-control" id="changwat_in_code">
+      <option>สกลนคร</option>
+      <option>กาฬสินธุ์</option>
+      <option>นครพนม</option>
+      <option>บึงกาฬ</option>
+    </select>
+    </div>
+ 
+    
 </form>
+</div>
 </div>
 
 </body>
