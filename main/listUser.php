d0bd1eccec61328aa0f60b3d4c9f911447e8113c
Thanasak1412 <Thanasaksrisaeng@gmail.com>
2020-12-24 23:25:08 +0700

    Refactor code

--- main/listUser.php
+++ main/listUser.php
@@ -1,11 +1,13 @@
 <?php
+  include('../include/config.php');
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
+<html lang="th">
+  <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>แสดงข้อมูลผู้ใช้งาน</title>
     <script src="../js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="https://getbootstrap.com/docs/4.1/content/tables/">
@@ -52,24 +54,35 @@
   });
 </script>
 
-<?php
-  include('../include/config.php');
-            
-  try {
-    $pdo = new PDO($query_string,$username,$password); 
-    // $query = 'SELECT * FROM user'; 
-    $query = 'SELECT * FROM user 
-      LEFT JOIN prename ON user.prename_id = prename.prename_id
-      LEFT JOIN office ON user.office_id =office.office_id
-      LEFT JOIN `status` ON user.status_id = status.status_id
-      LEFT JOIN `group` ON user.group_id = group.group_id';
+<?php 
+  // try {
+  //   $pdo = new PDO($query_string,$username,$password);
 
-    $q = $pdo->query($query);
-    $q -> setFetchMode(PDO::FETCH_ASSOC);
-    }
-    catch (PDOException $e) {
-      die("Could not connect to the database $dbname : " .$e->getMessage());
-    }
+  //   $query = 'SELECT * FROM user 
+  //     LEFT JOIN prename ON user.prename_id = prename.prename_id
+  //     LEFT JOIN office ON user.office_id =office.office_id
+  //     LEFT JOIN `status` ON user.status_id = status.status_id
+  //     LEFT JOIN `group` ON user.group_id = group.group_id';
+
+  //     $q = $pdo->query($query);
+  //     $q -> setFetchMode(PDO::FETCH_ASSOC);
+  // } catch (PDOException $e) {
+  //       die("Could not connect to the database $dbname : " .$e->getMessage());
+  // }
+  try {
+    $pdo = new PDO($query_string,$username,$password);
+
+    $sql = 'SELECT * FROM user 
+             LEFT JOIN prename ON user.prename_id = prename.prename_id
+             LEFT JOIN office ON user.office_id =office.office_id
+             LEFT JOIN `status` ON user.status_id = status.status_id
+             LEFT JOIN `group` ON user.group_id = group.group_id';
+    $obj = $pdo->prepare($sql);
+    $obj -> execute();
+    $obj -> setFetchMode(PDO::FETCH_ASSOC);
+  } catch (PDOException $e) {
+    die("Could not connect to the database $dbname : " . $e -> getMessage());
+  }
 ?>
 
   <h1>แสดงข้อมูลผู้ใช้งาน</h1>
@@ -94,19 +107,19 @@
 
         <tbody class="content">
           <?php
-            while($rows = $q -> fetch()):
+            while($rows = $obj -> fetch()):
             ?>
               <tr>
-                <th scope="row"><?php echo htmlspecialchars($rows['user_id']) ?></th>
-                <td><?php echo htmlspecialchars($rows['user_login']) ?></td>
-                <td><?php echo htmlspecialchars($rows['prename_id']) ?></td>
-                <td><?php echo htmlspecialchars($rows['fname']) ?></td>
-                <td><?php echo htmlspecialchars($rows['lname']) ?></td>
-                <td><?php echo htmlspecialchars($rows['phone']) ?></td>
-                <td><?php echo htmlspecialchars($rows['office_name']) ?></td>
-                <td><?php echo htmlspecialchars($rows['group_name']) ?></td>
-                <td><?php echo htmlspecialchars($rows['status_name']) ?></td>
-                <td><?php echo htmlspecialchars($rows['date_register']) ?></td>
+                <th scope="row"><?php echo $rows['user_id'] ?></th>
+                <td><?php echo $rows['user_login'] ?></td>
+                <td><?php echo $rows['prename_id'] ?></td>
+                <td><?php echo $rows['fname'] ?></td>
+                <td><?php echo $rows['lname'] ?></td>
+                <td><?php echo $rows['phone'] ?></td>
+                <td><?php echo $rows['office_name'] ?></td>
+                <td><?php echo $rows['group_name'] ?></td>
+                <td><?php echo $rows['status_name'] ?></td>
+                <td><?php echo $rows['date_register'] ?></td>
               </tr>
               <?php endwhile; ?>
         </tbody>
