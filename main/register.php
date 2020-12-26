87cb8f9af76f55413ac81f7ca5e55551bb875e7c
jackskn <smatchai.y@hotmail.co.th>
2020-12-26 16:29:51 +0700

    update

--- main/register.php
+++ main/register.php
@@ -54,7 +54,7 @@ function thaiMonthShort($x) {
 <body style="background-color: #b9ddff;  background-image: url(../image/header03.png); background-repeat: no-repeat; background-size: 500px; background-position: top right;">
 
 <script>
-var input_required=['fname','lname','cid','tel','changwat_out_code','ampur_out_code','tambon_out_code','ampur_in_code','tambon_in_code','date_to_sakonnakhon'];
+var input_required=['fname','lname','cid','tel','changwat_out_code','ampur_out_code','ampur_in_code','tambon_in_code','date_to_sakonnakhon'];
 $(document).ready(function () {
   $('.datepicker').datepicker({
       format: 'dd/mm/yyyy',
