47b78cd1e27424f50f26a72bfb97230d82ad4809
teetawat147 <54974898+teetawat147@users.noreply.github.com>
2020-12-26 16:35:09 +0700

    Update config.php
    1

--- include/config.php
+++ include/config.php
@@ -1,17 +1,11 @@
 <?php
-// return array(
-//     'host' => '203.157.177.7',
-//     'username' => 'root',
-//     'password' => '64127427',
-//     'port' => '3306',
-//     'db' => 'covid19register'
-// );
+
 $projectTitle="รายงานตัวเข้าสกลนคร";
-$host='203.157.177.7';
-$username='root';
-$password='64127427';
-$port='3306';
-$db='covid19register';
+$host='';
+$username='';
+$password='';
+$port='';
+$db='';
 
 
 $query_string="mysql:host=".$host.";dbname=".$db.";port=".$port;
