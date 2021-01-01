3ac6d69501320612b0da648cd0e6d941072866d2
teetawat147 <teetawat147@hotmail.com>
2021-01-01 14:02:16 +0700

    last evaluation risk

--- main/evaluation_risk.php
+++ main/evaluation_risk.php
@@ -34,30 +34,47 @@ include('../include/config.php');
   <body>
 <?php
 include("./header.php");
+$sql_report_risk="
+SELECT
+	r.risk_level_name,
+	r.risk_level_id,
+	count( * ) AS count_ampur_risk 
+FROM
+	ampur a
+	LEFT JOIN risk_level r ON a.risk_status_id = r.risk_level_id 
+GROUP BY
+	a.risk_status_id
+";
+$obj=$connect->prepare($sql_report_risk);
+$obj->execute();
+$rows_report_risk=$obj->fetchAll(PDO::FETCH_ASSOC);
+$risk_level_id = $rows_report_risk['risk_level_id'];
 ?>
 
-<main role="main" style="margin-top:60px;">
+<main role="main" style="margin-top:90px;">
     <div class="container">
-        <h5>เกณฑ์การประเมินความเสี่ยงรายบุคคล</h5>
+        <h5>เกณฑ์การประเมินความเสี่ยง covid-19 รายบุคคล</h5>
     </div>
+    <?php if ($_SESSION['group_id']>0){ ?>
     <img src="../image/covid_evaluate.png" class="rounded img-fluid" alt="...">
     <ul class="list-group">
+      <?php foreach ($rows_report_risk as $key => $value) { ?>
+      
+        <div class="tag-link"  risk_level_id = "<?php echo $value['risk_level_id']; ?>" >
         <li class="list-group-item d-flex justify-content-between align-items-center">
-            Cras justo odio
-            <span class="badge badge-primary badge-pill">14</span>
+            <?php echo $value['risk_level_name']   ?>
+          
+            <span class="badge badge-primary badge-pill"><?php echo $value['count_ampur_risk']  ?></span>
+          
         </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            Dapibus ac facilisis in
-            <span class="badge badge-primary badge-pill">2</span>
-        </li>
-        <li class="list-group-item d-flex justify-content-between align-items-center">
-            Morbi leo risus
-            <span class="badge badge-primary badge-pill">1</span>
-        </li>
+        </div>
+        <?php } ?>
     </ul>
 
-</main>
 
+    
+</main>
+<?php } ?>
 <div id="forExcelExport" style="display: none;"></div>
 
 <!-- FOOTER -->
@@ -69,4 +86,30 @@ include("./footer.php");
   window.jQuery || document.write('<script src="../js/jquery-3.2.1.min.js"><\/script>')
 </script>
 <script src="../js/bootstrap.bundle.min.js"></script>
+<script>
+
+       $(function(){
+            $(".tag-link").click(function(){
+              console.log($(this).attr("risk_level_id"));
+                if($(this).attr("risk_level_id")==0){ //เขียว
+                  //console.log($(this).attr("risk_level_id"));
+                  var form = $('<form action="./ampur_green.php" method="post"><input type="hidden" name="risk_level_id" value="' + $(this).attr("risk_level_id") + '"></input>' + '</form>');
+                
+                }else if($(this).attr("risk_level_id")==1){
+                  //console.log($(this).attr("risk_level_id"));
+                  var form = $('<form action="./ampur_yellow.php" method="post"><input type="hidden" name="risk_level_id" value="' + $(this).attr("risk_level_id") + '"></input>' + '</form>');
+                
+                }else if($(this).attr("risk_level_id")==2){
+                 //console.log($(this).attr("risk_level_id"));
+                  var form = $('<form action="./ampur_orange.php" method="post"><input type="hidden" name="risk_level_id" value="' + $(this).attr("risk_level_id") + '"></input>' + '</form>');
+                
+                }else{
+                  //console.log(attr("risk_level_id"));
+                  var form = $('<form action="./ampur_red.php" method="post"><input type="hidden" name="risk_level_id" value="' + $(this).attr("risk_level_id") + '"></input>' + '</form>');
+                }
+                $('body').append(form);
+                $(form).submit(); 
+            })
+        })
+      </script>
 </html>
