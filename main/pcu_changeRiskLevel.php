d8fa4bb8271deedb79f49b1c5ed8229c75fb3f88
khomsunt <khom_meanmon@hotmail.com>
2021-01-06 21:18:35 +0700

    edit empty date arrived leaved

--- main/pcu_changeRiskLevel.php
+++ main/pcu_changeRiskLevel.php
@@ -6,9 +6,9 @@ print_r($_POST);
 $risk_level_datetime=date("Y-m-d H:i:s");
 include('../include/config.php');
 $sql="update covid_register set 
-cut_status_id=".$_POST['cut_status_id'].",
-date_arrived_sakonnakhon='".$_POST['date_arrived_sakonnakhon']."',
-date_leaved_sakonnakhon='".$_POST['date_leaved_sakonnakhon']."',
+cut_status_id=".$_POST['cut_status_id'].",".(($_POST['date_arrived_sakonnakhon'])?"
+date_arrived_sakonnakhon='".$_POST['date_arrived_sakonnakhon']."',":"").
+(($_POST['date_leaved_sakonnakhon'])?"date_leaved_sakonnakhon='".$_POST['date_leaved_sakonnakhon']."',":"")."
 risk_level_user_id=".$_SESSION['user_id'].",
 risk_level_datetime='".$risk_level_datetime."' 
 where covid_register_id=".$_POST['covid_register_id'];
