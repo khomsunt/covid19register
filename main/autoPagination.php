37b5119268a6f8823b9b3bda1d6c0641bec5d4b1
khomsunt <khom_meanmon@hotmail.com>
2021-01-06 10:25:27 +0700

    auto pagination

---
+++ main/autoPagination.php
@@ -0,0 +1,17 @@
+<nav aria-label="Page navigation example">
+    <ul class="pagination justify-content-end">
+        <li class="page-item <?php echo ($page=="0")?"disabled":""; ?>">
+        <a class="page-link previous-pagination-link">Previous</a>
+        </li>
+        <?php
+        for ($p=0; $p < $pages; $p++) { 
+        ?>
+            <li class="page-item <?php echo ($page==$p)?"active":""; ?>"><a class="page-link pagination-link" page="<?php echo $p; ?>"><?php echo $p+1; ?></a></li>
+        <?php
+        }
+        ?>
+        <li class="page-item <?php echo ($page==($pages-1))?"disabled":""; ?>">
+        <a class="page-link next-pagination-link">Next</a>
+        </li>
+    </ul>
+</nav>
