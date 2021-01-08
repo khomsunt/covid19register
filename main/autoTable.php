b2eeead030dc13a4a3432d29699b7a72ab808d7f
jackskn <smatchai.y@hotmail.co.th>
2021-01-08 17:36:33 +0700

    update

--- main/autoTable.php
+++ main/autoTable.php
@@ -207,7 +207,7 @@
 <script src="../js/jquery-3.2.1.min.js" ></script>
 <script>window.jQuery || document.write('<script src="../js/jquery-3.2.1.min.js"><\/script>')</script><script src="../js/bootstrap.bundle.min.js"></script>
 <script src="../js/tableToCards.js"></script>
-<!-- <script src='../js/table2excel.js'></script> -->
+<script src='../js/table2excel.js'></script>
 <script>
 $(function () {
     $('[data-toggle="tooltip"]').tooltip();
