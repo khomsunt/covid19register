6b47617be5da2a8976be1f781b6a03e41600100e
khomsunt <khom_meanmon@hotmail.com>
2021-01-11 16:01:23 +0700

    link to person_list

--- main/autoTableTable.php
+++ main/autoTableTable.php
@@ -145,7 +145,7 @@
                             }
                             ?>
                             <td style="white-space: nowrap; text-align:<?php echo autoAlign($a_k[0]); ?>" >
-                                <div class="col_<?php echo $col; ?> <?php echo $a_k[3]; ?>" data-toggle="tooltip" data-placement="top" title="<?php echo str_replace("_"," ",$a_k[3]); ?>">
+                                <div class="col_<?php echo $col; ?> <?php echo $a_k[3]; ?>" originalValue="<?php echo $v; ?>" data-toggle="tooltip" data-placement="top" title="<?php echo str_replace("_"," ",$a_k[3]); ?>">
                                     <?php echo $thisValue; ?>
                                 </div>
                             </td>
